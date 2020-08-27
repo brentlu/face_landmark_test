004e65e48942781c93bc9b370d2c5c413489d1e0
Brent Lu <brent.lu@intel.com>
2020-08-27 14:27:19 +0800

    test_blink: implement class for csv accessing

--- test_blink.py
+++ test_blink.py
@@ -12,6 +12,93 @@ import process_video as pv
 #   1. draw left eye
 #   2. draw a green rectangle on the face if a blink is detected
 
+class FacialVideo:
+    def __init__(self, video_path):
+        # open the video file
+        self.cap = cv2.VideoCapture(video_path)
+
+        self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.fps = int(self.cap.get(cv2.CAP_PROP_FPS))
+        self.frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        self.hog_detector = dlib.get_frontal_face_detector()
+        self.rotation = pv.auto_detect_rotation(video_path, self.hog_detector)
+
+        if self.rotation == cv2.ROTATE_90_CLOCKWISE or self.rotation == cv2.ROTATE_90_COUNTERCLOCKWISE:
+            temp = self.width
+            self.width = self.height
+            self.height = temp
+
+        self.frame_index = 0
+
+        csv_path = pv.get_csv_data_file(video_path)
+
+        if csv_path != None:
+            if os.path.isfile(csv_path) == False:
+                # should not get here
+                return
+
+            # open the csv file
+            self.csvfile = open(csv_path, 'r', newline = '')
+            self.csv_reader = csv.DictReader(self.csvfile)
+
+            # read the first row
+            try:
+                self.__csv_row = next(self.csv_reader)
+                self.__csv_index = int(self.__csv_row['index'])
+            except StopIteration:
+                self.__csv_index = self.frame_count + 1
+
+    def __del__(self):
+        self.csvfile.close()
+        self.cap.release()
+
+    def read(self):
+        ret, frame = self.cap.read()
+        if ret == False:
+            # no frame left in the video
+            return ret, frame
+
+        self.frame_index += 1
+
+        if self.rotation != -1:
+            frame = cv2.rotate(frame, self.rotation)
+
+        if self.frame_index == self.__csv_index:
+            # this frame has csv data
+            self.time_stamp = float(self.__csv_row['time_stamp'])
+
+            self.landmarks = []
+
+            for n in range(0, 68):
+                x = int(self.__csv_row['mark_%d_x' % (n)])
+                y = int(self.__csv_row['mark_%d_y' % (n)])
+
+                self.landmarks.append((x, y))
+
+            self.rect = []
+
+            self.rect.append((int(self.__csv_row['target_left']), int(self.__csv_row['target_top'])))
+            self.rect.append((int(self.__csv_row['target_right']), int(self.__csv_row['target_bottom'])))
+
+            # read the next row
+            try:
+                self.__csv_row = next(self.csv_reader)
+                self.__csv_index = int(self.__csv_row['index'])
+            except StopIteration:
+                self.__csv_index = self.frame_count + 1
+
+        return ret, frame
+
+    def available(self):
+        if len(self.landmarks) != 68:
+            return False
+
+        if len(self.rect) != 2:
+            return False
+
+        return True
 
 def calculate_ear_value(landmarks, eye):
     if len(landmarks) != 68:
@@ -69,107 +156,46 @@ def test_blink(frame_index, ear_value):
 
 def main():
     input_video_path = './20200429_2B.mp4'
-    output_video_path = './test.mp4'
+    output_video_path = './test2.mp4'
 
-    csv_index = 0
-    frame_index = 0
- 
     times = []
     ears = []
 
-    csv_path = pv.get_csv_data_file(input_video_path)
-
-    if csv_path == None:
-        print('Fail to get cvs data for %s\n' % (input_video_path))
-        return
-
-    if os.path.isfile(csv_path) == False:
-        # should not get here
-        return
-
-    # open the video file
-    cap = cv2.VideoCapture(input_video_path)
-
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    hog_detector = dlib.get_frontal_face_detector()
-    rotation = pv.auto_detect_rotation(input_video_path, hog_detector)
-
-    if rotation == cv2.ROTATE_90_CLOCKWISE or rotation == cv2.ROTATE_90_COUNTERCLOCKWISE:
-        temp = width
-        width = height
-        height = temp
-
-    # open the csv file
-    csvfile = open(csv_path, 'r', newline = '')
-    csv_reader = csv.DictReader(csvfile)
-
-    # read the first row
-    try:
-        row = next(csv_reader)
-        csv_index = int(row['index'])
-    except StopIteration:
-        csv_index = frame_count + 1
+    fv = FacialVideo(input_video_path)
 
     # always use mp4
     video_writer = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'),
-                                   fps, (width, height))
+                                   fv.fps, (fv.width, fv.height))
 
     while True:
-        ret, frame = cap.read()
+        ret, frame = fv.read()
         if ret == False:
             # no frame to process
             break;
 
-        frame_index += 1
+        if fv.available() != False:
+            ear_left = calculate_ear_value(fv.landmarks, 'left')
+            ear_right = calculate_ear_value(fv.landmarks, 'right')
 
-        if rotation != -1:
-            frame = cv2.rotate(frame, rotation)
+            blink = test_blink(fv.frame_index, ear_left)
 
-        if frame_index == csv_index:
-            # this frame has csv data
-            landmarks = []
+            print('  frame: %3d, time_stamp: %4.3f, ear: (%4.3f,%4.3f), blink: %s' % (fv.frame_index, fv.time_stamp, ear_left, ear_right, str(blink)))
 
-            for n in range(0, 68):
-                x = int(row['mark_%d_x' % (n)])
-                y = int(row['mark_%d_y' % (n)])
-
-                landmarks.append((x, y))
-
-            time_stamp = float(row['time_stamp'])
-
-            ear_left = calculate_ear_value(landmarks, 'left')
-            ear_right = calculate_ear_value(landmarks, 'right')
-
-            blink = test_blink(frame_index, ear_left)
-
-            print('  frame: %3d, time_stamp: %4.3f, ear: (%4.3f,%4.3f), blink: %s' % (int(row['index']), time_stamp, ear_left, ear_right, str(blink)))
-
-            times.append(time_stamp)
+            times.append(fv.time_stamp)
             ears.append(ear_left)
 
             # draw left eye
             for n in range(0, 68):
                 if n >= 42 or n < 48:
-                    cv2.circle(frame, landmarks[n], 6, (255, 0, 0), -1)
+                    cv2.circle(frame, fv.landmarks[n], 6, (255, 0, 0), -1)
 
             # draw rect if blink found
             if blink != False:
-                (x1, y1) = (int(row['target_left']), int(row['target_top']))
-                (x2, y2) = (int(row['target_right']), int(row['target_bottom']))
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
-
-            # read the next row
-            try:
-                row = next(csv_reader)
-                csv_index = int(row['index'])
-            except StopIteration:
-                csv_index = frame_count + 1
+                cv2.rectangle(frame, fv.rect[0], fv.rect[1], (0, 255, 0), 3)
 
         video_writer.write(frame)
+
+    video_writer.release()
 
     if len(times) != 0:
         plt.plot(times, ears, 'r--')
@@ -177,10 +203,6 @@ def main():
         plt.xlabel('time', fontsize = 12)
         plt.ylabel('EAR', fontsize = 12)
         plt.show()
-
-    video_writer.release()
-    csvfile.close()
-    cap.release()
 
     return
 
