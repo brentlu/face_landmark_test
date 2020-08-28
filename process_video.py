faf86259c3f9fc823704ca0debf08f45e8fb72ba
Brent Lu <brent.lu@intel.com>
2020-08-28 21:09:42 +0800

    Implement training recipe support

--- process_video.py
+++ process_video.py
@@ -1,41 +1,77 @@
 #!/usr/bin/python3
 
 from facial_engine import FacialEngine
-
-#import csv
-#import cv2
-#import dlib
-#import hashlib
+import argparse
+import csv
 import magic
-#import numpy as np
 import os
 import re
-#import subprocess
-#import time
 
+
+def process_one_video(video_path, csv_policy):
+
+    _, filename = os.path.split(video_path)
+    print('process_one_video: %s' % (filename))
+
+    engine = FacialEngine(video_path)
+
+    ret = engine.configure(csv_policy = csv_policy)
+    if ret == False:
+        print('    fail to configure engine')
+        return False
+
+    ret = engine.process_video()
+    if ret == False:
+        print('  fail to process video')
+        return False
+
+    print('  success')
+    return True
+
+def process_training_csv(csv_path):
+
+    _, filename = os.path.split(csv_path)
+    print('process_training_csv: %s' % (filename))
+
+    with open(csv_path, 'r', newline = '') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            # update the csv if found
+            ret = process_one_video(row['file_name'], 'update')
+            if ret == False:
+                return False
+
+    print('  success')
+    return True
 
 def main():
-    #input_video_path = '/media'
-    #input_video_path = '/media/Temp_AIpose20200811'
-    #input_video_path = '20200429_2B.mp4'
-    input_video_path = '.'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help = 'path to a video file, a directory, or a training recipe file')
 
-    if os.path.isfile(input_video_path) != False:
+    args = parser.parse_args()
 
-        engine = FacialEngine(input_video_path)
+    input_video_path = args.path
+
+    print('Input path = %s' % (input_video_path))
+
+    _, ext = os.path.splitext(input_video_path)
+
+    if ext == '.csv':
+        # could be a training recipe
+        process_training_csv(input_video_path)
+
+    elif os.path.isfile(input_video_path) != False:
+        mime = magic.Magic(mime=True)
+
+        file_mine = mime.from_file(input_video_path)
+        if file_mine.find('video') == -1:
+            print('  not a video file')
+            return False
 
         # update the csv if found
-        ret = engine.configure(csv_policy = 'update')
-        if ret == False:
-            print('main: fail to configure engine')
-            return
-
-        ret = engine.process_video()
-        if ret == False:
-            print('main: fail to process video')
+        ret = process_one_video(input_video_path, 'update')
 
     elif os.path.isdir(input_video_path):
-
         # looking for any video file which name ends with a 'A' or 'B' character
         prog = re.compile(r'.*[AB]\..+')
 
@@ -51,23 +87,12 @@ def main():
                 if prog.match(file) == None:
                     continue
 
-                engine = FacialEngine(file_path)
-
-                #ret = engine.configure(csv_policy = 'abort')
-                ret = engine.configure(csv_policy = 'update')
-                if ret == False:
-                    print('main: fail to configure engine')
-
-                ret = engine.process_video()
-                if ret == False:
-                    print('main: fail to process video')
+                ret = process_one_video(file_path, 'abort')
 
     else:
-        print('Fail to process input path %s' % (input_video_path))
+        print('Unrecognized path')
 
     return
-
-
 
 if __name__ == '__main__':
     main()
